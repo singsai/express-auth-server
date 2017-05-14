76b4df58adf5e67abc75f730594ee4232e917774
Nick Tang <nick.tang@gmail.com>
2017-05-14 02:24:19 -0700

    Create authentication controller

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
+const Authentication = require('./controllers/authentication')
+
 module.exports = function(app) {
-	app.get('/', function(req, res, next) {
-		res.send(['what do you want', 'dammit'])
-	})
+	app.post('/signup', Authentication.signup)
 }
