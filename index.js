973a7ee8aa4132e3efc063faca3f9b2ce91d04b3
Nick Tang <nick.tang@gmail.com>
2017-05-14 02:20:00 -0700

    Require and connect mongodb

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ const morgan = require('morgan')
 
 const app = express()
 const router = require('./router')
+const mongoose = require('mongoose')
 
-const port = process.env.PORT || 3090
-const server = http.createServer(app)
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth')
 
+// App Setup
 app.use(morgan('combined'))
 app.use(bodyParser.json({ type: '*/*' }))
 router(app)
+
+// Server setup
+const port = process.env.PORT || 3090
+const server = http.createServer(app)
 
 server.listen(port)
 console.log('Server listening on', port)
