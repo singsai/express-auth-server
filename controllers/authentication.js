284f8bf0663c76832fcbd882261d653d59008511
Nick Tang <nick.tang@gmail.com>
2017-05-14 02:37:31 -0700

    Save user to database if email is unique

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,34 @@
+const User = require('../models/user')
+
 exports.signup = function(req, res, next) {
-	res.send({ success: 'true' })
+	const email = req.body.email
+	const password = req.body.password
+
+	// See if user w/ given email exists
+	User.findOne({ email: email}, function(err, existingUser) {
+		if (err) { return next(err) }
+
+		// If a user w/ email does exist, return an error
+		if (existingUser) {
+			return res.status(422).send({ error: 'Email is in use' })
+		}
+
+		// If a user w/ email does NOT exist, create and save user record
+		const user = new User({
+			email: email,
+			password: password
+		})
+
+		user.save(function(err){
+			if (err) {return next(err)}
+
+			// Respond to request indicating user was created		
+			res.json({ success: true })
+		})
+
+		
+	})
+
+
+
 }
